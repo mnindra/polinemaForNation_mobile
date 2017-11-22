d8586ec20b838b10ac41d5992c70b210f0b18e09
mnindra <mnindrazaka@gmail.com>
2017-11-22 15:32:42 +0700

    form validasi selesai

--- app/components/Pemesanan/index.js
+++ app/components/Pemesanan/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Container, Content, Form, Item, Label, Input, Button, Text, Icon} from 'native-base';
+import {View} from 'react-native';
 import {api} from '../../config/api';
 import styles from './styles';
 
@@ -15,7 +16,8 @@ export default class Pemesanan extends Component {
 			nama: '',
 			email: '',
 			telp: '',
-			isi: ''
+			isi: '',
+			errors: {}
 		};
 	}
 
@@ -24,7 +26,8 @@ export default class Pemesanan extends Component {
 			nama: '',
 			email: '',
 			telp: '',
-			isi: ''
+			isi: '',
+			errors: {}
 		});
 	}
 
@@ -42,8 +45,17 @@ export default class Pemesanan extends Component {
 				isi: this.state.isi
 			})
 		}).then(response => {
-			console.log(response);
-			this.bersihkanInputan();
+			if(response.status === 422) {
+				response.json().then(responseJson => {
+					console.log(responseJson);
+					this.setState({
+						errors: responseJson.errors
+					});
+					console.log(this.state);
+				});
+			} else {
+				this.bersihkanInputan();
+			}
 		});
 	}
 
@@ -52,31 +64,48 @@ export default class Pemesanan extends Component {
 			<Container style={styles.container}>
 				<Content>
 					<Form>
-						<Item floatingLabel>
+						<Item floatingLabel error={this.state.errors.hasOwnProperty('nama')}>
 							<Label>Nama</Label>
 							<Input
 								value={this.state.nama}
 								onChangeText={(nama) => this.setState({nama})}/>
 						</Item>
-						<Item floatingLabel>
+						<View style={styles.error}>
+							<Text style={styles.errorText}>{this.state.errors.hasOwnProperty('nama') ? this.state.errors.nama[0] : ''}</Text>
+						</View>
+
+						<Item floatingLabel error={this.state.errors.hasOwnProperty('email')}>
 							<Label>Email</Label>
 							<Input
+								keyboardType={'email-address'}
 								value={this.state.email}
 								onChangeText={(email) => this.setState({email})}/>
 						</Item>
-						<Item floatingLabel>
+						<View style={styles.error}>
+							<Text style={styles.errorText}>{this.state.errors.hasOwnProperty('email') ? this.state.errors.email[0] : ''}</Text>
+						</View>
+
+						<Item floatingLabel error={this.state.errors.hasOwnProperty('telp')}>
 							<Label>Nomor Telepon</Label>
 							<Input
+								keyboardType={'numeric'}
 								value={this.state.telp}
 								onChangeText={(telp) => this.setState({telp})}/>
 						</Item>
-						<Item floatingLabel last>
+						<View style={styles.error}>
+							<Text style={styles.errorText}>{this.state.errors.hasOwnProperty('telp') ? this.state.errors.telp[0] : ''}</Text>
+						</View>
+
+						<Item floatingLabel error={this.state.errors.hasOwnProperty('isi')} last>
 							<Label>Pesan</Label>
 							<Input
 								value={this.state.isi}
 								multiline={true}
 								onChangeText={(isi) => this.setState({isi})}/>
 						</Item>
+						<View style={styles.error}>
+							<Text style={styles.errorText}>{this.state.errors.hasOwnProperty('isi') ? this.state.errors.isi[0] : ''}</Text>
+						</View>
 					</Form>
 				</Content>
 				<Button
--- app/components/Pemesanan/styles.js
+++ app/components/Pemesanan/styles.js
@@ -3,6 +3,14 @@ import {StyleSheet} from 'react-native';
 const styles  = StyleSheet.create({
 	container: {
 		backgroundColor: '#fff'
+	},
+	error: {
+		paddingLeft: 15,
+		paddingRight: 15,
+		paddingTop: 10
+	},
+	errorText: {
+		color: '#ff5b51'
 	}
 });
 
